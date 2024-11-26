b22dc255f65c83d3e2e0662ac0f12bcb68c1b20a
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2024-11-26 19:58:43 +0100

    Search optimized - need to improve for pagination

--- AirportAutomation/AirportAutomationWeb/wwwroot/js/SearchHelper.js
+++ AirportAutomation/AirportAutomationWeb/wwwroot/js/SearchHelper.js
@@ -1,273 +1,100 @@
-﻿function searchByFNameOrLName(searchUrl, entityType, page = 1) {
+﻿function searchData(searchUrl, searchFunction, entityType, page = 1) {
     $('#searchButton').on('click', function () {
-        var firstName = $('#firstName').val();
-        var lastName = $('#lastName').val();
-        if ((!firstName || firstName.trim() === '') && (!lastName || lastName.trim() === '')) {
-            return;
-        }
-        var pageSize = 10;
-        var requestUrl = `${searchUrl}?firstName=${encodeURIComponent(firstName)}&lastName=${encodeURIComponent(lastName)}&page=${page}&pageSize=${pageSize}`;
+        var requestUrl = buildRequestUrl(searchFunction, searchUrl, page);
+        if (requestUrl) {
+            var tableHead = $('#tableHead');
+            var tableBody = $('#tableBody');
+            
+            $.ajax({
+                url: requestUrl,
+                type: 'GET',
+                success: function (data) {
+                    if (data.success !== true || !data.data || data.data.totalCount === 0) {
+                        $('#dataNotFoundContainer').show();
+                        showAlertInContainer('No data found.', 'danger');
+                        return;
+                    }
+                    $('#dataForm').show();
+                    $('#paginationContainer').show();
 
-        var tableHead = $('#tableHead');
-        var tableBody = $('#tableBody');
+                    tableHead.empty();
+                    tableBody.empty();
 
-        $.ajax({
-            url: requestUrl,
-            type: 'GET',
-            success: function (data) {
-                if (data.success !== true || !data.data || data.data.totalCount === 0) {
-                    $('#dataNotFoundContainer').show();
-                    showAlertInContainer('No data found.', 'danger');
-                    return;
+                    var fields = Object.keys(data.data.data[0]);
+                    createTableHead(tableHead, fields, entityType);
+
+                    var rowsData = data.data.data || [];
+                    $.each(rowsData, function (_, item) {
+                        createTableBody(item, tableBody, entityType);
+                    });
+                },
+                error: function (xhr, status, error) {
+                    console.error('Error:', status, error);
+                    console.error('Response:', xhr.responseText);
+                    showAlertInContainer('An error occurred while fetching data.', 'danger');
                 }
-                $('#dataForm').show();
-                $('#paginationContainer').show();
-
-                tableHead.empty();
-                tableBody.empty();
-
-                var fields = Object.keys(data.data.data[0]);
-                createTableHead(tableHead, fields, entityType);
-
-                var rowsData = data.data.data || [];
-                $.each(rowsData, function (_, item) {
-                    createTableBody(item, tableBody, entityType);
-                });
-            },
-            error: function (xhr, status, error) {
-                console.error('Error:', status, error);
-                console.error('Response:', xhr.responseText);
-                showAlertInContainer('An error occurred while fetching data.', 'danger');
-            }
-        });
+            });
+        }
+        else {
+            showAlertInContainer('No valid search parameters provided.', 'warning');
+        }
     });
 }
 
-function searchByName(searchUrl, entityType, page = 1) {
-    $('#searchButton').on('click', function () {
-        var searchTerm = $('#searchInput').val();
-        if (!searchTerm || searchTerm.trim() === '') {
-            return;
-        }
-        var pageSize = 10;
-        var requestUrl = `${searchUrl}?name=${encodeURIComponent(searchTerm)}&page=${page}&pageSize=${pageSize}`;
-        console.log(searchUrl);
-        console.log(requestUrl);
-        console.log(searchTerm);
+function buildRequestUrl(searchType, searchUrl, page) {
+    var pageSize = 10;
 
-        var tableHead = $('#tableHead');
-        var tableBody = $('#tableBody');
-
-        $.ajax({
-            url: requestUrl,
-            type: 'GET',
-            success: function (data) {
-                if (data.success !== true || !data.data || data.data.totalCount === 0) {
-                    $('#dataNotFoundContainer').show();
-                    showAlertInContainer('No data found.', 'danger');
-                    return;
-                }
-                $('#dataForm').show();
-                $('#paginationContainer').show();
-
-                tableHead.empty();
-                tableBody.empty();
-
-                var fields = Object.keys(data.data.data[0]);
-                createTableHead(tableHead, fields, entityType);
-
-                var rowsData = data.data.data || [];
-                $.each(rowsData, function (_, item) {
-                    createTableBody(item, tableBody, entityType);
-                });
-            },
-            error: function (xhr, status, error) {
-                console.error('Error:', status, error);
-                console.error('Response:', xhr.responseText);
-                showAlertInContainer('An error occurred while fetching data.', 'danger');
+    switch (searchType) {
+        case 'searchByFNameOrLName':
+            var firstName = $('#firstName').val();
+            var lastName = $('#lastName').val();
+            if ((!firstName || firstName.trim() === '') && (!lastName || lastName.trim() === '')) {
+                return null;
             }
-        });
-    });
-}
+            return `${searchUrl}?firstName=${encodeURIComponent(firstName)}&lastName=${encodeURIComponent(lastName)}&page=${page}&pageSize=${pageSize}`;
 
-function searchByDate(searchUrl, entityType, page = 1) {
-    $('#searchButton').on('click', function () {
-        var startDate = $('#startDate').val();
-        var endDate = $('#endDate').val();
-        if ((!startDate || startDate.trim() === '') && (!endDate || endDate.trim() === '')) {
-            return;
-        }
-        var pageSize = 10;
-        var requestUrl = `${searchUrl}?startDate=${encodeURIComponent(startDate)}&endDate=${encodeURIComponent(endDate)}&page=${page}&pageSize=${pageSize}`;
-
-        var tableHead = $('#tableHead');
-        var tableBody = $('#tableBody');
-
-        $.ajax({
-            url: requestUrl,
-            type: 'GET',
-            success: function (data) {
-                if (data.success !== true || !data.data || data.data.totalCount === 0) {
-                    $('#dataNotFoundContainer').show();
-                    showAlertInContainer('No data found.', 'danger');
-                    return;
-                }
-                $('#dataForm').show();
-                $('#paginationContainer').show();
-
-                tableHead.empty();
-                tableBody.empty();
-
-                var fields = Object.keys(data.data.data[0]);
-                createTableHead(tableHead, fields, entityType);
-
-                var rowsData = data.data.data || [];
-                $.each(rowsData, function (_, item) {
-                    createTableBody(item, tableBody, entityType);
-                });
-            },
-            error: function (xhr, status, error) {
-                console.error('Error:', status, error);
-                console.error('Response:', xhr.responseText);
-                showAlertInContainer('An error occurred while fetching data.', 'danger');
+        case 'searchByName':
+            var searchName = $('#searchInput').val();
+            if (!searchName || searchName.trim() === '') {
+                return null;
             }
-        });
-    });
-}
+            return `${searchUrl}?name=${encodeURIComponent(searchName)}&page=${page}&pageSize=${pageSize}`;
 
-function searchByPrice(searchUrl, entityType, page = 1) {
-    $('#searchButton').on('click', function () {
-        var minPrice = $('#minPrice').val();
-        var maxPrice = $('#maxPrice').val();
-        if (minPrice === null || minPrice === '' || maxPrice === null || maxPrice === '') {
-            return;
-        }
-        if (isNaN(minPrice) || isNaN(maxPrice)) {
-            return;
-        }
-        var pageSize = 10;
-        var requestUrl = `${searchUrl}?minPrice=${encodeURIComponent(minPrice)}&maxPrice=${encodeURIComponent(maxPrice)}&page=${page}&pageSize=${pageSize}`;
-
-        var tableHead = $('#tableHead');
-        var tableBody = $('#tableBody');
-
-        $.ajax({
-            url: requestUrl,
-            type: 'GET',
-            success: function (data) {
-                if (data.success !== true || !data.data || data.data.totalCount === 0) {
-                    $('#dataNotFoundContainer').show();
-                    showAlertInContainer('No data found.', 'danger');
-                    return;
-                }
-                $('#dataForm').show();
-                $('#paginationContainer').show();
-
-                tableHead.empty();
-                tableBody.empty();
-
-                var fields = Object.keys(data.data.data[0]);
-                createTableHead(tableHead, fields, entityType);
-
-                var rowsData = data.data.data || [];
-                $.each(rowsData, function (_, item) {
-                    createTableBody(item, tableBody, entityType);
-                });
-            },
-            error: function (xhr, status, error) {
-                console.error('Error:', status, error);
-                console.error('Response:', xhr.responseText);
-                showAlertInContainer('An error occurred while fetching data.', 'danger');
+        case 'searchByDate':
+            var startDate = $('#startDate').val();
+            var endDate = $('#endDate').val();
+            if ((!startDate || startDate.trim() === '') && (!endDate || endDate.trim() === '')) {
+                return null;
             }
-        });
-    });
-}
+            return `${searchUrl}?startDate=${encodeURIComponent(startDate)}&endDate=${encodeURIComponent(endDate)}&page=${page}&pageSize=${pageSize}`;
 
-function searchByCityOrAirport(searchUrl, entityType, page = 1) {
-    $('#searchButton').on('click', function () {
-        var city = $('#city').val();
-        var airport = $('#airport').val();
-        if ((!city || city.trim() === '') && (!airport || airport.trim() === '')) {
-            return;
-        }
-        var pageSize = 10;
-        var requestUrl = `${searchUrl}?city=${encodeURIComponent(city)}&airport=${encodeURIComponent(airport)}&page=${page}&pageSize=${pageSize}`;
-
-        var tableHead = $('#tableHead');
-        var tableBody = $('#tableBody');
-
-        $.ajax({
-            url: requestUrl,
-            type: 'GET',
-            success: function (data) {
-                if (data.success !== true || !data.data || data.data.totalCount === 0) {
-                    $('#dataNotFoundContainer').show();
-                    showAlertInContainer('No data found.', 'danger');
-                    return;
-                }
-                $('#dataForm').show();
-                $('#paginationContainer').show();
-
-                tableHead.empty();
-                tableBody.empty();
-
-                var fields = Object.keys(data.data.data[0]);
-                createTableHead(tableHead, fields, entityType);
-
-                var rowsData = data.data.data || [];
-                $.each(rowsData, function (_, item) {
-                    createTableBody(item, tableBody, entityType);
-                });
-            },
-            error: function (xhr, status, error) {
-                console.error('Error:', status, error);
-                console.error('Response:', xhr.responseText);
-                showAlertInContainer('An error occurred while fetching data.', 'danger');
+        case 'searchByPrice':
+            var minPrice = $('#minPrice').val();
+            var maxPrice = $('#maxPrice').val();
+            if ((!minPrice || minPrice.trim() === '') && (!maxPrice || maxPrice.trim() === '')) {
+                return null;
             }
-        });
-    });
-}
-
-function searchByRole(searchUrl, entityType, page = 1) {
-    $('#searchButton').on('click', function () {
-        var searchTerm = $('#roleSelect').val();
-        if (!searchTerm || searchTerm.trim() === '') {
-            return;
-        }
-        var pageSize = 10;
-        var requestUrl = `${searchUrl}?role=${encodeURIComponent(searchTerm)}&page=${page}&pageSize=${pageSize}`;
-
-        var tableHead = $('#tableHead');
-        var tableBody = $('#tableBody');
-
-        $.ajax({
-            url: requestUrl,
-            type: 'GET',
-            success: function (data) {
-                if (data.success !== true || !data.data || data.data.totalCount === 0) {
-                    $('#dataNotFoundContainer').show();
-                    showAlertInContainer('No data found.', 'danger');
-                    return;
-                }
-                $('#dataForm').show();
-                $('#paginationContainer').show();
-
-                tableHead.empty();
-                tableBody.empty();
-
-                var fields = Object.keys(data.data.data[0]);
-                createTableHead(tableHead, fields, entityType);
-
-                var rowsData = data.data.data || [];
-                $.each(rowsData, function (_, item) {
-                    createTableBody(item, tableBody, entityType);
-                });
-            },
-            error: function (xhr, status, error) {
-                console.error('Error:', status, error);
-                console.error('Response:', xhr.responseText);
-                showAlertInContainer('An error occurred while fetching data.', 'danger');
+            if (isNaN(minPrice) || isNaN(maxPrice)) {
+                return null;
             }
-        });
-    });
+            return `${searchUrl}?minPrice=${encodeURIComponent(minPrice)}&maxPrice=${encodeURIComponent(maxPrice)}&page=${page}&pageSize=${pageSize}`;
+
+        case 'searchByCityOrAirport':
+            var city = $('#city').val();
+            var airport = $('#airport').val();
+            if ((!city || city.trim() === '') && (!airport || airport.trim() === '')) {
+                return null;
+            }
+            return `${searchUrl}?city=${encodeURIComponent(city)}&airport=${encodeURIComponent(airport)}&page=${page}&pageSize=${pageSize}`;
+
+        case 'searchByRole':
+            var searchRole = $('#roleSelect').val();
+            if (!searchRole || searchRole.trim() === '') {
+                return null;
+            }
+            return `${searchUrl}?role=${encodeURIComponent(searchRole)}&page=${page}&pageSize=${pageSize}`;
+
+        default:
+            return null;
+    }
 }
